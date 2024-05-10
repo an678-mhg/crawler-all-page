9d0ee5621fe207ff9477f96e98253a2905888ba3
An Nguyen <an.nguyen@vnetwork.vn>
2024-05-10 11:41:03 +0700

    fix: bug

--- src/index.ts
+++ src/index.ts
@@ -1,94 +1,94 @@
-"use strict";
+'use strict';
 
-import { Logger } from "sitka";
-import puppeteer from "puppeteer";
-import _ from "lodash";
-import fs from "fs";
+import { Logger } from 'sitka';
+import puppeteer from 'puppeteer';
+import _ from 'lodash';
+import fs from 'fs';
 
 class Crawler {
-  private _logger: Logger;
-  private _url: string;
-  private _target: string;
-  private _ignorePages: string[] = [];
-  private _outDir: string = "out";
+	private _logger: Logger;
+	private _url: string;
+	private _target: string;
+	private _ignorePages: string[] = [];
+	private _outDir = 'out';
 
-  constructor(
-    url: string,
-    target: string,
-    ignorePages: string[] = [],
-    outDir: string = "out"
-  ) {
-    this._logger = Logger.getLogger({ name: this.constructor.name });
-    this._url = url;
-    this._target = target;
-    this._ignorePages = ignorePages;
-    this._outDir = outDir;
-  }
+	constructor(
+		url: string,
+		target: string,
+		ignorePages: string[] = [],
+		outDir = 'out'
+	) {
+		this._logger = Logger.getLogger({ name: this.constructor.name });
+		this._url = url;
+		this._target = target;
+		this._ignorePages = ignorePages;
+		this._outDir = outDir;
+	}
 
-  async run() {
-    this._logger.debug("Starting Crawler");
-    const browser = await puppeteer.launch({ headless: true });
+	async run() {
+		this._logger.debug('Starting Crawler');
+		const browser = await puppeteer.launch({ headless: true });
 
-    this._logger.debug(`Go to ${this._url}`);
-    const page = await browser.newPage();
-    await page.goto(this._url);
+		this._logger.debug(`Go to ${this._url}`);
+		const page = await browser.newPage();
+		await page.goto(this._url);
 
-    this._logger.debug("Get all a tags element");
-    const allHref = await page.evaluate(() => {
-      // @ts-ignore
-      return Array.from(document?.querySelectorAll("a")).map((a) => a.href);
-    });
+		this._logger.debug('Get all a tags element');
+		const allHref = await page.evaluate(() => {
+			// @ts-ignore
+			return Array.from(document?.querySelectorAll('a')).map((a) => a.href);
+		});
 
-    this._logger.debug("Start crawling with each path");
-    await page.close();
+		this._logger.debug('Start crawling with each path');
+		await page.close();
 
-    for (const href of _.uniq(allHref)) {
-      if (this._ignorePages.includes(href)) {
-        continue;
-      }
+		for (const href of _.uniq(allHref)) {
+			if (this._ignorePages.includes(href)) {
+				continue;
+			}
 
-      const page = await browser.newPage();
-      await page.goto(href);
+			const page = await browser.newPage();
+			await page.goto(href);
 
-      this._logger.debug(`Start crawling with ${href}`);
+			this._logger.debug(`Start crawling with ${href}`);
 
-      const content = await page.content();
+			const content = await page.content();
 
-      this._logger.debug(`Done crawling with ${href}`);
+			this._logger.debug(`Done crawling with ${href}`);
 
-      const directory = `${href
-        .replaceAll("//", "_")
-        .replaceAll("/", "_")}.html`;
+			const directory = `${href
+				.replaceAll('//', '_')
+				.replaceAll('/', '_')}.html`;
 
-      if (!fs.existsSync(this._outDir)) {
-        fs.mkdirSync(this._outDir, { recursive: true });
-      }
+			if (!fs.existsSync(this._outDir)) {
+				fs.mkdirSync(this._outDir, { recursive: true });
+			}
 
-      fs.writeFileSync(`${this._outDir}/${directory}`, content, "utf-8");
+			fs.writeFileSync(`${this._outDir}/${directory}`, content, 'utf-8');
 
-      await page.close(); // Đóng trang sau khi hoàn thành việc crawling
-    }
+			await page.close(); // Đóng trang sau khi hoàn thành việc crawling
+		}
 
-    browser.close();
-  }
+		browser.close();
+	}
 }
 
 async function start() {
-  const crawler = new Crawler(
-    "https://gcore.com/docs/cloud",
-    ".scully-container",
-    [
-      "https://gcore.com/docs/cloud",
-      "https://api.gcore.com/docs",
-      "https://gcore.com/",
-      "https://gcore.com/docs/search",
-      "https://gcore.com/docs",
-    ],
-    "out"
-  );
-  crawler.run();
+	const crawler = new Crawler(
+		'https://gcore.com/docs/cloud',
+		'.scully-container',
+		[
+			'https://gcore.com/docs/cloud',
+			'https://api.gcore.com/docs',
+			'https://gcore.com/',
+			'https://gcore.com/docs/search',
+			'https://gcore.com/docs',
+		],
+		'out'
+	);
+	crawler.run();
 }
 
 start().catch((err) => {
-  console.log(err);
+	console.log(err);
 });
